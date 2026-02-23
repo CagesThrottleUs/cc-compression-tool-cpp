2adf8dfc1afd3962490bca3eaf3dcdf2df17c54d
CagesThrottleUs <manstein.felix@gmail.com>
2026-02-23 07:51:28 +0530

    feat(exceptions): add base model for exceptions
    Currently, we have created a base model for exceptions with a base
    exception class that inherits constructor from the base exception class.
    
    This commit introduces only one derived exception class:
    argument_exception.
    
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ src/exceptions/exceptions.cpp
@@ -0,0 +1,9 @@
+#include "argument_exception.hpp"
+#include "base_exception.hpp"
+
+namespace exceptions {
+
+base_exception::~base_exception() = default;
+argument_exception::~argument_exception() = default;
+
+}  // namespace exceptions
