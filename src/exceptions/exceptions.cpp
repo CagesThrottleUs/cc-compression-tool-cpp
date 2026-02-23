84b9a2ee27dffa6bb5d0ce529e4710a66f5aaa32
CagesThrottleUs <manstein.felix@gmail.com>
2026-02-23 09:13:14 +0530

    feat(exceptions): add file operation exception class
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

--- src/exceptions/exceptions.cpp
+++ src/exceptions/exceptions.cpp
@@ -1,9 +1,10 @@
 #include "argument_exception.hpp"
 #include "base_exception.hpp"
+#include "file_operation_exception.hpp"
 
 namespace exceptions {
 
 base_exception::~base_exception() = default;
 argument_exception::~argument_exception() = default;
-
+file_operation_exception::~file_operation_exception() = default;
 }  // namespace exceptions
