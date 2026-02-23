c2534e6d011cdc967598afe7a3e962df74964d84
CagesThrottleUs <manstein.felix@gmail.com>
2026-02-23 07:52:44 +0530

    feat(argument): add argument handling as the first step
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ src/argument/arguments.cpp
@@ -0,0 +1,32 @@
+#include <filesystem>
+#include <span>
+
+#include "../exceptions/argument_exception.hpp"
+#include "argument.hpp"
+
+namespace argument {
+
+void validate_arguments(const argv_view& args_view) {
+  std::span<char*> args(args_view);
+
+  // Must match exact number of arguments
+  if (args.size() < 2 || args.size() > 3) {
+    throw exceptions::argument_exception(
+        "Usage: " + std::string(args[0]) +
+        " <text_file_path> <optional_output_file_path>");
+  }
+
+  // Input file must exist
+  if (!std::filesystem::exists(args[1])) {
+    throw exceptions::argument_exception("Input file does not exist: " +
+                                         std::string(args[1]));
+  }
+
+  // output file must not exist
+  if (args.size() == 3 && std::filesystem::exists(args[2])) {
+    throw exceptions::argument_exception("Output file already exists: " +
+                                         std::string(args[2]));
+  }
+}
+
+}  // namespace argument
