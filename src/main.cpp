33743708176be9619470aed65b56f613df92cc7f
CagesThrottleUs <manstein.felix@gmail.com>
2026-02-23 16:23:17 +0530

    refactor: add prefix codes usage to main function
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "exit_codes.hpp"
 #include "file_handler/input_file.hpp"
 #include "frequency_table/frequency_table.hpp"
+#include "prefix_codes/prefix_codes.hpp"
 
 auto main(int argc, char** argv) -> int {
   argument::argv_view args(argv, static_cast<std::size_t>(argc));
@@ -16,6 +17,11 @@ auto main(int argc, char** argv) -> int {
     auto table = frequency_table::build_frequency_table(
         file_handler::load_file(validated_args[1]));
     frequency_table::print_frequency_table(table);
+    auto prefixes = prefix_codes::generate_prefix_codes(table);
+    for (const auto& [codepoint, prefix] : prefixes) {
+      std::cout << file_handler::codepoint_to_utf8(codepoint) << " " << prefix
+                << '\n';
+    }
   } catch (const exceptions::argument_exception& e) {
     std::cerr << e.what() << '\n';
     return std::to_underlying(exit_codes::exit_code::argument_error);
