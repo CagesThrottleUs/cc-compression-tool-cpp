88bec8c82fd91d7f226d8dd1a80aa9884e764137
CagesThrottleUs <manstein.felix@gmail.com>
2026-02-23 14:30:11 +0530

    refactor: update main.cpp to use frequency table
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,16 @@
 #include "exceptions/file_operation_exception.hpp"
 #include "exit_codes.hpp"
 #include "file_handler/input_file.hpp"
+#include "frequency_table/frequency_table.hpp"
 
 auto main(int argc, char** argv) -> int {
   argument::argv_view args(argv, static_cast<std::size_t>(argc));
 
   try {
     auto validated_args = argument::validate_arguments(args);
-    auto input = file_handler::load_file(validated_args[1]);
+    auto table = frequency_table::build_frequency_table(
+        file_handler::load_file(validated_args[1]));
+    frequency_table::print_frequency_table(table);
   } catch (const exceptions::argument_exception& e) {
     std::cerr << e.what() << '\n';
     return std::to_underlying(exit_codes::exit_code::argument_error);
